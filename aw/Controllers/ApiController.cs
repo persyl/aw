c5c9043d23539d1509c589b277c5e923b628e5da
Per Lundkvist <persyl@gmail.com>
2016-01-31 17:43:12 +0100

    Cannot use Csharp 6 features on AppHarbor

--- aw/Controllers/ApiController.cs
+++ aw/Controllers/ApiController.cs
@@ -3,12 +3,12 @@ using System.Web.Mvc;
 
 namespace Aw.Controllers
 {
-    public class ApiController:Controller
+    public class ApiController : Controller
     {
         [HttpPost]
         public JsonResult Result(QuizAnswer data)
         {
-            return Json(new { ServerResponse = $"Tack {data.Contestant}, dina svar är nu registrerade" }, JsonRequestBehavior.DenyGet);
+            return Json(new { ServerResponse = string.Format("Tack {0}, dina svar är nu registrerade.", data.Contestant) }, JsonRequestBehavior.DenyGet);
         }
     }
 }
